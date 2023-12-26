c809883df31c6713fe5d75e7b8faa259aef8d30a
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-26 09:31:35 +0000

    find all references

--- neovim-config/lua/lewis/remap.lua
+++ neovim-config/lua/lewis/remap.lua
@@ -35,3 +35,10 @@ vim.keymap.set("n", "<C-c>", "<Esc>")
 
 -- don't let me accidentally press 'Q' ever
 vim.keymap.set("n", "Q", "<nop>")
+
+-- [[ LSP ]]
+
+-- Find All References
+vim.keymap.set('n', '<leader>fr', ':lua vim.lsp.buf.references()<CR>', {
+	silent = true, noremap = true
+})
