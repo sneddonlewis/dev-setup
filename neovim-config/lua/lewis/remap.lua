2c80706ceb65f8bc064e2bbee78082e77d108c09
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-26 09:25:57 +0000

    remap.lua for key remappings

---
+++ neovim-config/lua/lewis/remap.lua
@@ -0,0 +1,37 @@
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Move things highlighted in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- half page up and down with centered cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- paste over but keep the original yanked to paste again
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- remap ctrl-c to escape
+vim.keymap.set("n", "<C-c>", "<Esc>")
+
+-- don't let me accidentally press 'Q' ever
+vim.keymap.set("n", "Q", "<nop>")
--- neovim-config/lua/lewis/set.lua
+++ neovim-config/lua/lewis/set.lua
@@ -39,25 +39,3 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
-
--- Move things highlighted in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- half page up and down with centered cursor
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- paste over but keep the original yanked to paste again
-vim.keymap.set("x", "<leader>p", "\"_dP")
-
--- yank to clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-
--- remap ctrl-c to escape
-vim.keymap.set("n", "<C-c>", "<Esc>")
-
--- don't let me accidentally press 'Q' ever
-vim.keymap.set("n", "Q", "<nop>")
