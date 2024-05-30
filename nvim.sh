e92513989a3fc46d6ddc5121cc8b4e6c41f7d092
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-30 19:18:52 +0100

    updated dev setup to use my latest tmux config

--- nvim.sh
+++ nvim.sh
@@ -5,13 +5,14 @@ nvim_config="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 # set Neovim config path for Windows
 if [ "$(expr substr $(uname -s) 1 5)" = "MINGW" ]; then
     nvim_config="$USERPROFILE/AppData/Local/nvim"
+    # Todo want to use the current "neovim-config" on windows
+    # but use git@github.com:sneddonlewis/nvim.git for Mac/Linux
+    # that needs rust compiler installed as well as ripgrep and some other stuff
+    # I put it in the readme in that github repo so check there
 fi
 
 rm -rf "$nvim_config"
-
 mkdir -p "$nvim_config"
-
 cp -R ./neovim-config/* "$nvim_config"
-
 nvim --headless "+Lazy! sync" +qa
 
--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 tmux_config="${XDG_CONFIG_HOME:-$HOME}/.tmux.conf"
 tmux_start_script="${XDG_CONFIG_HOME:-$HOME}/.local/bin/tmux.sh"
+tmux_plugins="${XDG_CONFIG_HOME:-$HOME}/.tmux"
 
 # Check if the file already exists
 if [ -f "$tmux_config" ]; then
@@ -12,6 +13,13 @@ else
     echo "No existing tmux config file found."
 fi
 
+if [ -f "$tmux_plugins" ]; then
+    echo "Removing existing tmux config file ..."
+    rm "$tmux_plugins"
+else
+    echo "No existing tmux plugins found."
+fi
+
 if [ -f "$tmux_start_script" ]; then
     # If it exists, remove it with a friendly message
     echo "Removing existing tmux start script ..."
@@ -20,6 +28,15 @@ else
     # If it does not exist, display a different message
     echo "No existing tmux start script found."
 fi
+
+echo "making directory for tmux plugins"
+mkdir -p "$tmux_plugins/plugins"
+echo "cloning tpm into plugins"
+git clone https://github.com/tmux-plugins/tpm "$tmux_plugins/plugins/tpm"
+
+# could let tpm handle this but may as well do it at the same time
+echo "cloning tmux2k into plugins"
+git clone git@github.com:2KAbhishek/tmux2k.git "$tmux_plugins/plugins/tmux2k"
 
 # Copy the new tmux config file into place
 # Make sure to use 'tmux_config' variable, not 'nvim_config'
